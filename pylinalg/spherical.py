a2dcc6a887d2051024970b2d5451dfa51687337e
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-09-21 10:51:02 +0200

    clean out the repo

--- pylinalg/spherical.py
+++
@@ -1,7 +0,0 @@
-class Spherical:
-    def __init__(
-        self, radius: float = 1.0, phi: float = 0.0, theta: float = 0.0
-    ) -> None:
-        self.radius = radius
-        self.phi = phi
-        self.theta = theta
