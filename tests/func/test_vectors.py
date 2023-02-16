074326b313abb98f3393cb0ee7756a034e67fcd8
Sebastian Wallkötter <sebastian@wallkoetter.net>
2023-02-16 11:01:14 +0100

    test sensitive once more

--- tests/func/test_vectors.py
+++ tests/func/test_vectors.py
@@ -105,7 +105,7 @@ def test_vector_distance_between(vector_a, vector_b):
     expected = np.linalg.norm(vector_a - vector_b)
     result = pla.vector_distance_between(vector_a, vector_b)
 
-    assert np.allclose(result, expected, rtol=1e-10)
+    assert np.allclose(result, expected, rtol=1e-16)
 
 
 def test_vector_distance_between_exceptions():
