31b3e0bbd4d225c73b96e426c3b2f09b0ac3475e
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-04-26 14:31:15 +0200

    get version with importlib

--- pylinalg/__init__.py
+++ pylinalg/__init__.py
@@ -5,9 +5,10 @@ Linear algebra utilities for Python.
 """
 
 # flake8: noqa
+from importlib.metadata import version  
 
-__version__ = "0.3.4"
 
+__version__ = version('pylinalg') 
 version_info = tuple(map(int, __version__.split(".")))
 
 
