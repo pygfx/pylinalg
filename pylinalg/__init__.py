f91918a9adde5152a1069ac267b83d599f38fec5
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2023-05-07 20:55:33 +0200

    make public api cleaner and test for it

--- pylinalg/__init__.py
+++ pylinalg/__init__.py
@@ -7,15 +7,13 @@ Linear algebra utilities for Python.
 # flake8: noqa
 from importlib.metadata import version
 
-
 __version__ = version("pylinalg")
 version_info = tuple(map(int, __version__.split(".")))
 
 
-from . import matrix, misc, quaternion, vector
 from .matrix import *
 from .misc import *
 from .quaternion import *
 from .vector import *
 
-__all__ = matrix.__all__ + misc.__all__ + quaternion.__all__ + vector.__all__
+del version
